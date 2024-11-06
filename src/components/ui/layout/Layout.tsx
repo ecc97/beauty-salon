5bcb82acd89fd21c6a7ebde6da0ac61448a745e1
ecc97 <carmonaedwin1123@gmail.com>
2024-11-05 20:38:25 -0500

    chore: header and sidebar adjusts

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Sidebar from "../organisms/sidebar/Sidebar";
+import Header from "../organisms/header/Header";
 interface ILayoutProps {
     children: React.ReactNode;
 }
@@ -7,7 +8,7 @@ interface ILayoutProps {
 export default function Layout({ children }: ILayoutProps) {
     return (
         <div className="flex h-screen bg-gray-100">
-            <Sidebar />
+            <Header />
             <main className="flex-1 p-8 overflow-auto">
                 {children}
             </main>
--- src/components/ui/organisms/header/Header.tsx
+++ src/components/ui/organisms/header/Header.tsx
@@ -1,16 +1,16 @@
 "use client"
 import React from 'react'
-import Button from '../../atoms/button/Button'
-import { signOut } from 'next-auth/react'
 import Sidebar from '../sidebar/Sidebar'
+import './header.scss'
 
 export default function Header() {
-    const handleSignOut = () => {
-        signOut()
-    }
+
     return (
-        <header className='w-full flex justify-between items-center p-2'>
-            {/* <Button type='button' onClick={handleSignOut} className=' text-white border border-slate-500 bg-cyan-700 rounded-lg hover:bg-cyan-300 p-4'>Cerrar Sesión</Button> */}
+        <header className='header w-64 bg-gray-800 text-white'>
+            <div className='flex items-center mb-8'>
+                <h1 className='font-bold'>Beauty Sala</h1>
+            </div>
+            <Sidebar />
         </header>
     )
 }
